99f5285b4a4dc9b226a5098143d8859827c6dfb8
Kipngetich222 <123062517+Kipngetich222@users.noreply.github.com>
2023-06-09 08:17:31 +0300

    simple login and signup pages

---
+++ Home.js
@@ -0,0 +1,14 @@
+//alert("Nitakupiga bro")
+// function bonyeza(){
+
+//     document.getElementById("alafu").innerHTML = "Helo JavaScript"
+// }
+// function mzee(){
+// //     document.getElementById("myImage").src="./1.jpg"
+// // }
+// // function mpya(){
+// //     document.getElementById("myImage").src="./2.jpg"
+// }
+function addition(){
+  console.log(  5+6 )
+}
